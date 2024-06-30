db898eb771c16112c75a09314075984e1bc3d4f9
19746 <mattiabraga2006@gmail.com>
2024-06-30 11:08:34 +0200

    Making the creation more accurate, Tolman-Oppenheimer-Volkoff my men

--- bin/bh_creation.py
+++ bin/bh_creation.py
@@ -7,6 +7,8 @@ Neetre 2024
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from scipy.integrate import odeint
+
 
 
 # Const
@@ -14,35 +16,53 @@ G = 6.67430e-11
 c = 299792458
 M_s = 1.989e30
 R_s = 6.957e8
+sigma_SB = 5.670e-8  # Stefan-Boltzmann constant
+a_rad = 4 * sigma_SB / c
+G_F = 1.436e-62  # Fermi constant
 
-initial_mass = 1000 * M_s  # kg
-initial_rad = 50 * R_s  # m
-initial_temp = 40000  # K
+initial_mass = 40 * M_s  # kg
+initial_rad = 20 * R_s  # m
+initial_temp = 1e7  # K
+omega = 1e-4
+B_0 = 1e8
+
 
 # Simultaion param
 t_max = 1000
 dt = 0.1
+N = 200
 
 
 t = np.arange(0, t_max, dt)
-mass = np.zeros_like(t)
-radius = np.zeros_like(t)
-temp = np.zeros_like(t)
+mass = np.linspace(0, initial_mass, N)
+radius = np.zeros((len(t), N))
+temp = np.zeros((len(t), N))
+rho = np.zeros((len(t), N))
+P = np.zeros((len(t), N))
+Y_e = np.ones((len(t), N)) * 0.5
+X = np.ones((len(t), N)) * 0.7
 
 
-mass[0] = initial_mass
-radius[0] = initial_rad
-temp[0] = initial_temp
+radius[0] = np.linspace(1e-10, initial_rad, N)
+rho[0] = initial_mass / (4/3 * np.pi * initial_rad**3) * (1 - (radius[0]/initial_rad)**2)
+temp[0] = initial_temp * (1 - 0.9 * (radius[0]/initial_rad))
+P[0] = G * initial_mass**2 / (8 * np.pi * initial_rad**4) * (1 - (radius[0]/initial_rad)**2)
 
 
-def mass_loss_rate(m, r, T):
-    return 1e-8 * (m / M_s) * (r / R_s) * (T / 10000) * M_s / t_max
+def TOV_equations(y, m_r, M, R):
+    r, P = y
+    rho = P / (G * M**2 / (8 * np.pi * R**4))  # Simplified EOS
+    drdm = 1 / (4 * np.pi * rho * r**2)
+    dPdm = -(G * m_r * rho / r**2) * (1 + P / (rho * c**2)) * (1 + 4 * np.pi * r**3 * P / (m_r * c**2)) / (1 - 2 * G * m_r / (r * c**2))
+    return [drdm, dPdm]
 
-def update_rad(m, r):
-    return max(r * (m / initial_mass)**0.5, 1e-10)  # Prevent radius from becoming zero
 
-def update_temp(m, r):
-    return initial_temp * (m / initial_mass)**0.5 * (initial_temp / r)**0.5
+def update_rad(rho, T, r, dm):
+    kappa = 0.2 * (1 + X[0]) + 0.1  # Thomson + heavy elements
+    P_rad = a_rad * T**4 / 3
+    L_rad = -64 * np.pi * sigma_SB * r**4 * T**3 * np.gradient(T, dm) / (3 * kappa * rho)
+    return P_rad, L_rad
+
 
 
 bh_formed = False
